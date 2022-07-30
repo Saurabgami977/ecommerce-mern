85f1c2d6b1291f3d22ed23eef026bfb29fa86036
Saurab Gami <saurabgami977@gmail.com>
2022-07-30 10:47:59 +0545

    dasd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,25 @@
-import { useEffect } from "react";
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import WebFont from "webfontloader";
-
-import "./App.css";
-import Header from "./component/layout/Header/Header.js";
-import Footer from "./component/layout/Footer/Footer";
-import Home from "./component/Home/Home";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	useEffect(() => {
-		WebFont.load({
-			google: {
-				families: ["Roboto", "Droid Sans", "Chilanka"],
-			},
-		});
-	}, []);
-
-	return (
-		<Router>
-			<Header />
-			<Routes>
-				<Route exact path="/" element={<Home />} />
-			</Routes>
-			<Footer />
-		</Router>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;

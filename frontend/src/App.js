0b2496c35c8557e04fc7fd90e2ba1287ded16cfb
Saurab Gami <saurabgami977@gmail.com>
2022-08-16 16:06:54 +0545

    my profile/account page made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WebFont from "webfontloader";
 
 import "./App.css";
+import store from "./store";
 import Footer from "./components/Layout/Footer/Footer";
 import Header from "./components/Layout/Header/Header";
 import Home from "./components/Home/Home";
@@ -10,8 +11,7 @@ import ProductDetails from "./components/Product/ProductDetails.js";
 import Products from "./components/Product/Products.js";
 import Search from "./components/Product/Search.js";
 import LoginSignup from "./components/User/LoginSignup";
-import "./store";
-import store from "./store";
+import Profile from "./components/User/Profile";
 import { loadUser } from "./store/actions/userAction";
 
 function App() {
@@ -32,6 +32,8 @@ function App() {
 				<Route exact path="/product/:id" element={<ProductDetails />} />
 				<Route exact path="/products" element={<Products />} />
 				<Route path="/products/:keyword" element={<Products />} />
+
+				<Route exact path="/account" element={<Profile />} />
 
 				<Route exact path="/search" element={<Search />} />
 				<Route exact path="/login" element={<LoginSignup />} />

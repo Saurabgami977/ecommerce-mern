a3dddfb026f5cb8a33b3b40e10ebd3b9e535479c
Saurab Gami <saurabgami977@gmail.com>
2022-08-18 22:20:10 +0545

    making it ready to deploy on heroku

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,16 @@ const cookieParser = require("cookie-parser");
 var cors = require("cors");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const path = require("path");
 
 const app = express();
 
 const errorMiddleware = require("./middleware/error");
+
+//Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+	require("dotenv").config({ path: "backend/config/config.env" });
+}
 
 app.use(express.json());
 app.use(cookieParser());
@@ -26,6 +32,11 @@ const user = require("./routes/userRoute");
 
 app.use("/api/v1", products);
 app.use("/api/v1", user);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 //Middleware for error
 app.use(errorMiddleware);

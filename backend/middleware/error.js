da5afc36120d1e39f27b35a20dc9cf8834ebaf31
Saurab Gami <saurabgami977@gmail.com>
2022-08-19 17:21:27 +0545

    change to procfile

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,4 +1,4 @@
-const ErrorHandler = require("../utils/errorhander");
+const ErrorHandler = require("../utils/errorHander");
 
 module.exports = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;

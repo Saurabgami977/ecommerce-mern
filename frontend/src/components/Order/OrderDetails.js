9ee8575dd3d0941c0998cc4f1d19b488d898f77d
Saurab Gami <saurabgami977@gmail.com>
2022-08-25 21:37:29 +0545

    order detail page

--- frontend/src/components/Order/OrderDetails.js
+++ frontend/src/components/Order/OrderDetails.js
@@ -34,27 +34,89 @@ const OrderDetails = () => {
 			{loading ? (
 				<Loader />
 			) : (
-				<div className="orderDetailContainer">
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-					<h1>{order?.orderItems && order.orderItems[0].name}</h1>
-				</div>
+				<>
+					<MetaData title="Order Details - Saurav Store" />
+					<div className="orderDetailsPage">
+						<div className="orderDetailsContainer">
+							<Typography component="h1">
+								Order #{order && order._id}
+							</Typography>
+							<div className="orderDetailsContainerBox">
+								<div>
+									<p>Name:</p>
+									<span>{order.user && order.user.name}</span>
+								</div>
+								<div>
+									<p>Phone:</p>
+									<span>
+										{order.shippingInfo && order.shippingInfo.phoneNo}
+									</span>
+								</div>
+								<div>
+									<p>Address:</p>
+									<span>
+										{order.shippingInfo &&
+											`${order.shippingInfo.address}, ${order.shippingInfo.city},
+										${order.shippingInfo.state}, ${order.shippingInfo.pinCode},
+										${order.shippingInfo.country}`}
+									</span>
+								</div>
+							</div>
+							<Typography>Payment</Typography>
+							<div className="orderDetailsContainerBox">
+								<div>
+									<p
+										className={
+											order.paymentInfo &&
+											order.paymentInfo.status === "succeeded"
+												? "greenColor"
+												: "redColor"
+										}
+									>
+										{order.paymentInfo &&
+										order.paymentInfo.status === "succeeded"
+											? "Paid"
+											: "Not Paid"}
+									</p>
+								</div>
+								<div>
+									<p>Amount:</p>
+									<span>$ {order.totalPrice && order.totalPrice}</span>
+								</div>
+							</div>
+							<Typography>Order Status</Typography>
+							<div className="orderDetailsContainerBox">
+								<div>
+									<p
+										className={
+											order.orderStatus && order.orderStatus === "Delivered"
+												? "greenColor"
+												: "redColor"
+										}
+									>
+										{order.orderStatus && order.orderStatus}
+									</p>
+								</div>
+							</div>
+						</div>
+						<div className="orderDetailsCartItems">
+							<Typography>Order Items: </Typography>
+							<div className="orderDetailsCartItemsContainer">
+								{order.orderItems &&
+									order.orderItems.map((item) => (
+										<div key={item.product}>
+											<img src={item.image} alt={item.name} />
+											<Link to={`/product/${item.product}`}>{item.name}</Link>
+											<span>
+												{item.quantity} X $ {item.price} =
+												<b>$ {item.price * item.quantity}</b>
+											</span>
+										</div>
+									))}
+							</div>
+						</div>
+					</div>
+				</>
 			)}
 		</>
 	);

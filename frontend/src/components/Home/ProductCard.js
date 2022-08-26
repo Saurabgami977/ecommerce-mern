a6016755d4486987f993886d9130c595ee179037
Saurab Gami <saurabgami977@gmail.com>
2022-08-26 22:52:35 +0545

    react rating used, style fixes

--- frontend/src/components/Home/ProductCard.js
+++ frontend/src/components/Home/ProductCard.js
@@ -1,17 +1,13 @@
+import { Rating } from "@mui/material";
 import React from "react";
 
 import { Link } from "react-router-dom";
 
-import ReactStars from "react-rating-stars-component";
-
 const ProductCard = ({ product }) => {
 	const options = {
-		edit: false,
-		color: "rgba(20,20,20,0.1",
-		activeColot: "tomato",
-		size: window.innerWidth < 600 ? 20 : 25,
 		value: product.ratings,
-		isHalf: true,
+		readOnly: true,
+		precision: 0.5,
 	};
 
 	return (
@@ -19,8 +15,10 @@ const ProductCard = ({ product }) => {
 			<img src={product.images[0].url} alt={product.name} />
 			<p>{product.name}</p>
 			<div>
-				<ReactStars {...options} />{" "}
-				<span>({product.numOfReviews} reviews)</span>
+				<Rating size="small" {...options} />{" "}
+				<span className="productCardSpan">
+					({product.numOfReviews} reviews)
+				</span>
 			</div>
 			<span>$ {product.price}</span>
 		</Link>
